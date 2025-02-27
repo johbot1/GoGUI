0be93541cc6c21a23bdc54cb134157de2ac24fe6
John <jbotonakis@carroll.edu>
2025-02-26 18:27:48 -0700

    Simplified the Update() funciton, just make it a little cleaner

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/vector"
 	"image/color"
 	"log"
 	"math/big"
@@ -20,18 +19,19 @@ func (g *Game) Update() error {
 	// Handling input (button clicks)
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
 		mouseX, mouseY := ebiten.CursorPosition()
-		if mouseX < 150 && mouseY > 50 && mouseY < 90 { // 1d4 button
-			g.selectedDice = 4
-		} else if mouseX < 150 && mouseY > 100 && mouseY < 140 { // 1d6 button
-			g.selectedDice = 6
-		} else if mouseX < 150 && mouseY > 150 && mouseY < 190 { // 1d8 button
-			g.selectedDice = 8
-		} else if mouseX < 150 && mouseY > 200 && mouseY < 240 { // 1d10 button
-			g.selectedDice = 10
-		} else if mouseX < 150 && mouseY > 250 && mouseY < 290 { // 1d20 button
-			g.selectedDice = 20
-		} else if mouseX < 150 && mouseY > 300 && mouseY < 340 { // 1d100 button
-			g.selectedDice = 100
+		switch {
+		case mouseX < 150 && mouseY > 50 && mouseY < 90:
+			g.selectedDice = 4 // 1d4 button
+		case mouseX < 150 && mouseY > 100 && mouseY < 140:
+			g.selectedDice = 6 // 1d6 button
+		case mouseX < 150 && mouseY > 150 && mouseY < 190:
+			g.selectedDice = 8 // 1d8 button
+		case mouseX < 150 && mouseY > 200 && mouseY < 240:
+			g.selectedDice = 10 // 1d10 button
+		case mouseX < 150 && mouseY > 250 && mouseY < 290:
+			g.selectedDice = 20 // 1d20 button
+		case mouseX < 150 && mouseY > 300 && mouseY < 340:
+			g.selectedDice = 100 // 1d100 button
 		}
 	}
 	return nil
@@ -50,43 +50,21 @@ func RollDice(sides int) int {
 // DrawDiceShape dynamically creates different dice outlines
 func DrawDiceShape(screen *ebiten.Image, x, y, size float32, sides int, lineWidth float32, color color.Color) {
 	scaleFactor := float32(1.5)
-	size *= scaleFactor
+	scaledSize := size * scaleFactor
 
 	switch sides {
 	case 4: // d4 - Triangle
-		vector.StrokeLine(screen, x, y+size, x+size/2, y, lineWidth, color, true)
-		vector.StrokeLine(screen, x+size/2, y, x+size, y+size, lineWidth, color, true)
-		vector.StrokeLine(screen, x+size, y+size, x, y+size, lineWidth, color, true)
-
+		DrawTriangle(screen, x, y, scaledSize, lineWidth, color)
 	case 6: // d6 - Square
-		vector.StrokeLine(screen, x, y, x+size, y, lineWidth, color, true)
-		vector.StrokeLine(screen, x, y+size, x+size, y+size, lineWidth, color, true)
-		vector.StrokeLine(screen, x, y, x, y+size, lineWidth, color, true)
-		vector.StrokeLine(screen, x+size, y, x+size, y+size, lineWidth, color, true)
-
+		drawSquare(screen, x, y, scaledSize, lineWidth, color)
 	case 8: // d8 - Diamond
-		vector.StrokeLine(screen, x+size/2, y, x, y+size/2, lineWidth, color, true)
-		vector.StrokeLine(screen, x, y+size/2, x+size/2, y+size, lineWidth, color, true)
-		vector.StrokeLine(screen, x+size/2, y+size, x+size, y+size/2, lineWidth, color, true)
-		vector.StrokeLine(screen, x+size, y+size/2, x+size/2, y, lineWidth, color, true)
-
+		drawDiamond(screen, x, y, scaledSize, lineWidth, color)
 	case 10: // d10 - Pentagon
-		vector.StrokeLine(screen, x+size/2, y, x, y+size/3, lineWidth, color, true)
-		vector.StrokeLine(screen, x, y+size/3, x+size/4, y+size, lineWidth, color, true)
-		vector.StrokeLine(screen, x+size/4, y+size, x+3*size/4, y+size, lineWidth, color, true)
-		vector.StrokeLine(screen, x+3*size/4, y+size, x+size, y+size/3, lineWidth, color, true)
-		vector.StrokeLine(screen, x+size, y+size/3, x+size/2, y, lineWidth, color, true)
-
+		drawPentagon(screen, x, y, scaledSize, lineWidth, color)
 	case 20: // d20 - Hexagon
-		vector.StrokeLine(screen, x+size/4, y, x+3*size/4, y, lineWidth, color, true)
-		vector.StrokeLine(screen, x+3*size/4, y, x+size, y+size/2, lineWidth, color, true)
-		vector.StrokeLine(screen, x+size, y+size/2, x+3*size/4, y+size, lineWidth, color, true)
-		vector.StrokeLine(screen, x+3*size/4, y+size, x+size/4, y+size, lineWidth, color, true)
-		vector.StrokeLine(screen, x+size/4, y+size, x, y+size/2, lineWidth, color, true)
-		vector.StrokeLine(screen, x, y+size/2, x+size/4, y, lineWidth, color, true)
-
+		drawHexagon(screen, x, y, scaledSize, lineWidth, color)
 	case 100: // d100 - Circle
-		//vector.DrawCircle(screen, x+size/2, y+size/2, size/2, color, true)
+		//vector.DrawCircle(screen, x+scaledSize/2, y+scaledSize/2, scaledSize/2, color, true)
 	}
 }
 
