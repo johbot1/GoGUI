0b0cf4ca10d4f806020b4306d6bcc11ca7ac120e
John <jbotonakis@carroll.edu>
2025-03-05 19:42:12 -0700

    Moved constants to a larger constant declaration for cleaner look; Removed RollDice function

--- constants.go
+++ constants.go
@@ -1,36 +1,25 @@
 package main
 
 import (
-	"crypto/rand"
 	"image/color"
-	"math/big"
 )
 
 // DiceSwitchingButtonColor Simply sets the background color of the rectangles behind each button
 var DiceSwitchingButtonColor = color.RGBA{R: 160, G: 155, B: 155, A: 255}
 
-var DiceRollingButtonColor = color.RGBA{R: 170, G: 175, B: 65, A: 255}
+var DiceRollingButtonColor = color.RGBA{R: 255, G: 26, B: 26, A: 255}
 
 // DiceSwitchingButtonXpos specifies the X positional value of a button
-const DiceSwitchingButtonXpos = float32(20)
 const (
+	DiceSwitchingButtonXpos   = float32(20)
 	DiceSwitchingButtonHeight = 40
 	DiceSwitchingButtonWidth  = 120
+	ScaleFactor               = float32(1.5)
+	DiceSwitchingButtonTitle  = int(DiceSwitchingButtonXpos + 20)
+	CircleDrawingXModifier    = 1.87
+	CircleDrawingYModifier    = 2.8
+	ButtonPlacementModifier   = 50
 )
-const ScaleFactor = float32(1.5)
-const DiceSwitchingButtonTitle = int(DiceSwitchingButtonXpos + 20)
-const CircleDrawingXModifier = 1.87
-const CircleDrawingYModifier = 2.7
 
 // Buttons Labels for the buttons (dice options)
 var Buttons = []string{"1d4", "1d6", "1d8", "1d10", "1d20", "1d100"}
-
-// RollDice generates a cryptographically secure random number for the dice roll
-func RollDice(sides int) int {
-	n, err := rand.Int(rand.Reader, big.NewInt(int64(sides)))
-	if err != nil {
-		// Fallback: Return max value in case of error (should rarely happen)
-		return sides
-	}
-	return int(n.Int64()) + 1
-}
