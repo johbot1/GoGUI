76c0143536ae44c08d920edb9917520f61cbe4a2
John <jbotonakis@carroll.edu>
2025-03-12 15:51:18 -0600

    Added color vars for multiplier buttons and color buttons; Organized constants into sections for easier readability

--- constants.go
+++ constants.go
@@ -9,20 +9,49 @@ var DiceSwitchingButtonColor = color.RGBA{R: 160, G: 155, B: 155, A: 255}
 
 var DiceRollingButtonColor = color.RGBA{R: 255, G: 26, B: 26, A: 255}
 
-// DiceSwitchingButtonXpos specifies the X positional value of a button
+var multiplierButtonColor = color.RGBA{80, 80, 80, 255} // Example color; adjust as desired
+
+var buttonColors = []color.RGBA{ // Define colors for the color buttons
+	{255, 0, 0, 255},     // Red
+	{0, 255, 0, 255},     // Green
+	{0, 0, 255, 255},     // Blue
+	{255, 255, 255, 255}, // White (for the X button)
+}
+
+// Constants applied to various parts of the code, grouped by their location
 const (
+	// Dice Switching Button Parameters
 	DiceSwitchingButtonXpos   = float32(20)
 	DiceSwitchingButtonHeight = 40
 	DiceSwitchingButtonWidth  = 120
-	ScaleFactor               = float32(1.5)
 	DiceSwitchingButtonTitle  = int(DiceSwitchingButtonXpos + 20)
-	CircleDrawingXModifier    = 1.87
-	CircleDrawingYModifier    = 2.8
-	ButtonPlacementModifier   = 50
-	RollButtonXpos            = 20
-	RollButtonYpos            = 500
-	RollButtonTextXpos        = 65
-	RollButtonTextYpos        = 550
+	// Dice Drawing Parameters
+	ScaleFactor             = float32(1.5)
+	CircleDrawingXModifier  = 1.82
+	CircleDrawingYModifier  = 2.5
+	ButtonPlacementModifier = 50
+	// Roll Dice ButtonParameters
+	RollButtonXpos     = 20
+	RollButtonYpos     = 500
+	RollButtonTextXpos = 65
+	RollButtonTextYpos = 550
+	RollButtonWidth    = 120
+	RollButtonHeight   = 80
+	// Font Parameters
+	FontSize = 25
+	FontDPI  = FontSize * 3
+	//Multiple Dice Button Parameters
+	MultiplierButtonX       = float32(20)
+	MultiplierButtonYStart  = float32(350) // Start drawing below the dice selection buttons
+	MultiplierButtonWidth   = float32(120)
+	MultiplierButtonHeight  = float32(40)
+	MultiplierButtonSpacing = float32(50) // Vertical spacing between buttons
+	//Color Changing Button Parameters
+	RightButtonYStart  = float32(100) // Start vertically around 100px from the top
+	RightButtonWidth   = float32(100) // Button width
+	RightButtonHeight  = float32(100) // Button height
+	RightButtonSpacing = float32(20)  // Space between buttons
+
 )
 
 // Buttons Labels for the buttons (dice options)
